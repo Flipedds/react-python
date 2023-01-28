8e00337f6c87c774eb43ce81f84194d245c91cf6
Filipe André <flipeandre12@gmail.com>
2023-01-27 23:51:26 -0300

    first commit

---
+++ frotend/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {useState, useEffect} from 'react';
+
+function App() {
+
+  const [articles, setArticles] = useState([])
+  
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/get', {
+    'methods':'GET',
+    headers: {
+      'Content-Type':'aplication/json'
+    }
+    })
+    .then(resp => resp.json())
+    .then(resp => setArticles(resp))
+    .catch(error => console.log(error))
+  },[])
+
+  return (
+    <div className="App">
+      <h1>Flask python e ReactJs</h1>
+      {articles.map(article => {
+        return(
+            <div key = {article.id}>
+              <h2>{article.title}</h2>
+              <p>{article.body}</p>
+              <p>{article.date}</p>
+            </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
